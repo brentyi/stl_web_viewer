48e731317ad7ad633c0a9e055e244b749f4c8b41
Brent Yi <yibrenth@gmail.com>
2017-01-01 07:17:41 +0000

    mongodb integration

--- modelhandler.js
+++ modelhandler.js
@@ -1,5 +1,5 @@
 const multer = require('multer');
-
+const mongodb = require('mongodb');
 
 var storage = multer.diskStorage({
     destination: (request, file, callback) => {
@@ -12,21 +12,58 @@ var storage = multer.diskStorage({
 });
 var upload = multer({storage: storage}).single('model');
 
+var db;
+var db_models;
+
+var db_url = 'mongodb://localhost:27017/stl_web_viewer';
+var MongoClient = mongodb.MongoClient;
+
+// Use connect method to connect to the Server
+MongoClient.connect(db_url, (err, database) => {
+    if (err) {
+        console.error('Unable to connect to the mongoDB server. Error:', err);
+    }
+    console.log('Connection established to', db_url);
+    db = database;
+    db_models = db.collection('models');
+});
+
 module.exports.uploadModel = (request, response) => {
-    request.fsname = 'model-' + Date.now() + '.stl';
+    var model_name = Date.now();
+    request.fsname = 'model-' + model_name + '.stl';
     console.log('Starting file upload');
     upload(request, response, (err) => {
         if(err) {
             console.log('Error Occured: ', err);
             return;
         }
+
         console.log('Uploaded model to ' + request.file.path);
 
         var host = (request.headers['x-forwarded-host'] || request.header.host);
-        response.json({
-            url: 'http://' + host + '/uploads/' + request.file.filename
+        var url = 'http://' + host + '/uploads/' + request.file.filename;
+
+        var date = new Date();
+        var expiration = new Date(date.setTime( date.getTime() + 1/*days*/ * 86400000 ));
+        db_models.insert({
+            name: model_name,
+            url: url,
+            path: request.file.path,
+            expiration: expiration
         });
-        console.log(request);
-        //response.json(request);
+
+        response.json({
+            url: url
+        });
     })
 }
+
+module.exports.getModel = (name, cb) => {
+    var model = db_models.findOne({name: name}, (err, item) => {
+        console.log(err);
+        console.log(item);
+
+        cb(item);
+    });
+}
+
--- routes/viewer.js
+++ routes/viewer.js
@@ -3,13 +3,15 @@ const handler = require('../modelhandler');
 const router = express.Router();
 
 /* Viewer */
-router.get('/:id', (req, res, next) => {
+router.get('/:name', (req, res, next) => {
     console.log(req.params);
-    var param = {
-        styles: ['viewer'],
-        model_url: 'https://brentyi.com/public/LeggedBot_Reduced.stl'
-    };
-    res.render('viewer', param);
+    handler.getModel(parseInt(req.params.name), (data) => {
+        var param = {
+            styles: ['viewer'],
+            model_url: data.url
+        };
+        res.render('viewer', param);
+    });
 });
 
 module.exports = router;
