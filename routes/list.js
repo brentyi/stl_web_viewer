ddfc9ca3c2ba085b5b1eee5dd09694bc6c43c34f
Brent Yi <yibrenth@gmail.com>
2017-04-15 10:30:49 +0000

    flexbox formatting

--- routes/list.js
+++ routes/list.js
@@ -9,6 +9,9 @@ router.get('/', (req, res, next) => {
             obj.info_url = handler.site_root + '/info/' + obj.name;
             var date = new Date(obj.name);
             obj.formatted_name = date.toLocaleString("en-US", {timeZone: "America/Los_Angeles"});
+
+            if (!obj.original_filename)
+                obj.original_filename = "(empty)";
         });
 
         var param = {
